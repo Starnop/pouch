df3063ed1cbc5754fcc68105173acfc22ea0469e
Frank Yang <yyb196@gmail.com>
2018-04-20 15:55:17 +0800

    判断创建容器方是不是sigma以及是否需要去掉容器名字前面的/
    Signed-off-by: Frank Yang <yyb196@gmail.com>

---
+++ pkg/utils/tls.go
@@ -0,0 +1,39 @@
+package utils
+
+import (
+	"context"
+	"strings"
+)
+
+// SetTlsIssuer set issuer name of tls to context
+func SetTlsIssuer(ctx context.Context, issuer string) context.Context {
+	return context.WithValue(ctx, "pouch.server.tls.issuer", issuer)
+}
+
+// SetTlsIssuer fetch issuer name from context
+func GetTlsIssuer(ctx context.Context) string {
+	issuer := ctx.Value("pouch.server.tls.issuer")
+	if issuer == nil {
+		return ""
+	}
+	return issuer.(string)
+}
+
+// SetTlsCommonName set common name of tls to context
+func SetTlsCommonName(ctx context.Context, cn string) context.Context {
+	return context.WithValue(ctx, "pouch.server.tls.cn", cn)
+}
+
+// GetTlsCommonName fetch common name from context
+func GetTlsCommonName(ctx context.Context) string {
+	issuer := ctx.Value("pouch.server.tls.cn")
+	if issuer == nil {
+		return ""
+	}
+	return issuer.(string)
+}
+
+// IsSigma checks tls name and return if this context is owned by sigma
+func IsSigma(ctx context.Context) bool {
+	return GetTlsIssuer(ctx) == "ali" && strings.Contains(GetTlsCommonName(ctx), "sigma")
+}
