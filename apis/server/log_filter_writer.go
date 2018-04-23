4cc13f483e5cf9e4c761af17fb6aaf1c4b76e92c
Frank Yang <yyb196@gmail.com>
2018-04-23 17:17:48 +0800

    减少日志中TLS相关的日志，比如这样的：2018/04/23 16:19:50 http: TLS handshake error from 127.0.0.1:60173: tls: first record does not look like a TLS handshake
    Signed-off-by: Frank Yang <yyb196@gmail.com>

---
+++ apis/server/log_filter_writer.go
@@ -0,0 +1,22 @@
+package server
+
+import (
+	"bytes"
+	"io"
+	"os"
+)
+
+type filterLogWriter struct {
+	io.Writer
+}
+
+var stdFilterLogWriter filterLogWriter
+var filterString = []byte("http: TLS handshake error from")
+
+func (w filterLogWriter) Write(p []byte) (n int, err error) {
+	if bytes.Contains(p, filterString) {
+		return 0, nil
+	} else {
+		return os.Stderr.Write(p)
+	}
+}
