be13a228e394d507d6c8f450d2f88cfbb3445e67
Rudy Zhang <rudyflyzhang@gmail.com>
2018-04-26 16:33:07 +0800

    set driver alias when driver is alilocal
    Signed-off-by: Rudy Zhang <rudyflyzhang@gmail.com>

--- hook_plugin/volume_hook.go
+++ hook_plugin/volume_hook.go
@@ -6,10 +6,17 @@ type VPlugin int
 
 var VolumePlugin VPlugin
 
-func (d VPlugin) PreVolumeCreate(config *types.VolumeCreateConfig) error {
-	if config.Driver == "alilocal" {
-		config.Driver = "local"
+func (v VPlugin) PreVolumeCreate(config *types.VolumeCreateConfig) error {
+	if config.Driver != "alilocal" {
+		return nil
 	}
+	config.Driver = "local"
+
+	// set driver alias in local.
+	if config.DriverOpts == nil {
+		config.DriverOpts = make(map[string]string)
+	}
+	config.DriverOpts["driver.alias"] = "alilocal"
 
 	return nil
 }
