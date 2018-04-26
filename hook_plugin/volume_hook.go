d750077817f09d2316668581f42d2fd2e05b1c7a
Rudy Zhang <rudyflyzhang@gmail.com>
2018-04-26 21:27:06 +0800

    remove changing alilocal to local in plugin hook
    Signed-off-by: Rudy Zhang <rudyflyzhang@gmail.com>

--- hook_plugin/volume_hook.go
+++ hook_plugin/volume_hook.go
@@ -7,16 +7,5 @@ type VPlugin int
 var VolumePlugin VPlugin
 
 func (v VPlugin) PreVolumeCreate(config *types.VolumeCreateConfig) error {
-	if config.Driver != "alilocal" {
-		return nil
-	}
-	config.Driver = "local"
-
-	// set driver alias in local.
-	if config.DriverOpts == nil {
-		config.DriverOpts = make(map[string]string)
-	}
-	config.DriverOpts["driver.alias"] = "alilocal"
-
 	return nil
 }
