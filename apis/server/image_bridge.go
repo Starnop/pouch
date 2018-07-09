72739f8315addb7260eba7daaa4038be20efea86
沈陵 <shenling.yyb@taobao.com>
2018-07-09 17:53:37 +0800

    去掉兼容sigma用docker client拉镜像的逻辑

--- apis/server/image_bridge.go
+++ apis/server/image_bridge.go
@@ -5,7 +5,6 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"strings"
 	"time"
@@ -14,18 +13,12 @@ import (
 	"github.com/alibaba/pouch/apis/types"
 	"github.com/alibaba/pouch/daemon/mgr"
 	"github.com/alibaba/pouch/pkg/httputils"
-	"github.com/alibaba/pouch/pkg/utils"
-
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 )
 
 // pullImage will pull an image from a specified registry.
 func (s *Server) pullImage(ctx context.Context, rw http.ResponseWriter, req *http.Request) error {
-	if utils.IsStale(ctx, req) {
-		return s.pullImageStale(ctx, rw, req)
-	}
-
 	image := req.FormValue("fromImage")
 	tag := req.FormValue("tag")
 
@@ -58,149 +51,6 @@ func (s *Server) pullImage(ctx context.Context, rw http.ResponseWriter, req *htt
 		return nil
 	}
 	return nil
-}
-
-type progressInfo struct {
-	Ref       string
-	Status    string
-	Offset    int64
-	Total     int64
-	StartedAt time.Time
-	UpdatedAt time.Time
-
-	// For Error handling
-	Code         int    // http response code
-	ErrorMessage string // detail error information
-}
-
-type JSONError struct {
-	Code    int    `json:"code,omitempty"`
-	Message string `json:"message,omitempty"`
-}
-
-type JSONProgress struct {
-	terminalFd uintptr
-	Current    int64 `json:"current,omitempty"`
-	Total      int64 `json:"total,omitempty"`
-	Start      int64 `json:"start,omitempty"`
-}
-
-type JSONMessage struct {
-	Stream          string        `json:"stream,omitempty"`
-	Status          string        `json:"status,omitempty"`
-	Progress        *JSONProgress `json:"progressDetail,omitempty"`
-	ProgressMessage string        `json:"progress,omitempty"` //deprecated
-	ID              string        `json:"id,omitempty"`
-	From            string        `json:"from,omitempty"`
-	Time            int64         `json:"time,omitempty"`
-	TimeNano        int64         `json:"timeNano,omitempty"`
-	Error           *JSONError    `json:"errorDetail,omitempty"`
-	ErrorMessage    string        `json:"error,omitempty"` //deprecated
-	// Aux contains out-of-band data, such as digests for push signing.
-	Aux *json.RawMessage `json:"aux,omitempty"`
-}
-
-// pullImageStale will pull an image from a specified registry.
-func (s *Server) pullImageStale(ctx context.Context, rw http.ResponseWriter, req *http.Request) error {
-	image := req.FormValue("fromImage")
-	tag := req.FormValue("tag")
-
-	if image == "" {
-		err := fmt.Errorf("fromImage cannot be empty")
-		return httputils.NewHTTPError(err, http.StatusBadRequest)
-	}
-
-	if tag == "" {
-		tag = "latest"
-		if index := strings.LastIndex(image, ":"); index > 0 {
-			tag = image[index+1:]
-			image = image[:index]
-		}
-	}
-	// record the time spent during image pull procedure.
-	defer func(start time.Time) {
-		metrics.ImagePullSummary.WithLabelValues(image + ":" + tag).Observe(metrics.SinceInMicroseconds(start))
-	}(time.Now())
-
-	// get registry auth from Request header
-	authStr := req.Header.Get("X-Registry-Auth")
-	authConfig := types.AuthConfig{}
-	if authStr != "" {
-		data := base64.NewDecoder(base64.URLEncoding, strings.NewReader(authStr))
-		if err := json.NewDecoder(data).Decode(&authConfig); err != nil {
-			return err
-		}
-	}
-
-	///////////start/////////
-	pipeR, pipeW := io.Pipe()
-	retChan := make(chan error)
-	go func() {
-		dec := json.NewDecoder(pipeR)
-		if _, err := dec.Token(); err != nil {
-			retChan <- fmt.Errorf("failed to read the opening token: %v", err)
-			return
-		}
-		encoder := json.NewEncoder(rw)
-		for dec.More() {
-			var infos []progressInfo
-
-			if err := dec.Decode(&infos); err != nil {
-				dec.Token()
-				retChan <- fmt.Errorf("failed to decode: %v", err)
-				return
-			}
-			if len(infos) > 0 {
-				for _, oneInfo := range infos {
-					if oneInfo.ErrorMessage != "" {
-						dec.Token()
-						retChan <- fmt.Errorf("pull image %s error %s", image, oneInfo.ErrorMessage)
-						return
-					}
-					switch oneInfo.Status {
-					case "downloading", "uploading":
-					case "resolving", "waiting":
-						fallthrough
-					default:
-						continue
-					}
-					err := encoder.Encode(&JSONMessage{
-						Status: oneInfo.Status,
-						ID:     oneInfo.Ref,
-						Progress: &JSONProgress{
-							Current: oneInfo.Offset,
-							Start:   0,
-							Total:   oneInfo.Total,
-						},
-					})
-					if err != nil {
-						dec.Token()
-						retChan <- err
-						return
-					}
-				}
-			}
-		}
-		dec.Token()
-		close(retChan)
-	}()
-	///////////end/////////
-
-	// Error information has be sent to client, so no need call resp.Write
-	if err := s.ImageMgr.PullImage(ctx, image+":"+tag, &authConfig, pipeW); err != nil {
-		logrus.Errorf("failed to pull image %s:%s: %v", image, tag, err)
-		return changeNotFoundAsSigmaNeed(err)
-	}
-	pipeW.Close()
-
-	return changeNotFoundAsSigmaNeed(<-retChan)
-}
-
-func changeNotFoundAsSigmaNeed(err error) error {
-	if err != nil && strings.HasSuffix(err.Error(), "not found") {
-		return fmt.Errorf(strings.Replace(err.Error(), "not found", "manifest unknown", -1))
-	}
-	return err
 }
 
 func (s *Server) getImage(ctx context.Context, rw http.ResponseWriter, req *http.Request) error {
