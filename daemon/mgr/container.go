109db1c8cba88c0cb1bd0da2959817f131ef4404
Michael Wan <zirenwan@gmail.com>
2018-08-20 01:56:32 -0400

    bugfix: merge env wrong when create container
    Signed-off-by: Michael Wan <zirenwan@gmail.com>

--- daemon/mgr/container.go
+++ daemon/mgr/container.go
@@ -1048,46 +1048,14 @@ func (mgr *ContainerManager) Update(ctx context.Context, name string, config *ty
 	}
 
 	// Update Env
-	if len(config.Env) > 0 {
-		newEnvMap, err := opts.ParseEnv(config.Env)
-		if err != nil {
-			return errors.Wrapf(err, "failed to parse new env")
-		}
-
-		oldEnvMap, err := opts.ParseEnv(c.Config.Env)
-		if err != nil {
-			return errors.Wrapf(err, "failed to parse old env")
-		}
-
-		for k, v := range newEnvMap {
-			// key should not be empty
-			if k == "" {
-				continue
-			}
-
-			// add or change an env
-			if v != "" {
-				oldEnvMap[k] = v
-				continue
-			}
-
-			// value is empty, we need delete the env
-			if _, exists := oldEnvMap[k]; exists {
-				delete(oldEnvMap, k)
-			}
-		}
-
-		newEnvSlice := []string{}
-		for k, v := range oldEnvMap {
-			newEnvSlice = append(newEnvSlice, fmt.Sprintf("%s=%s", k, v))
-		}
-
-		c.Config.Env = newEnvSlice
-
-		if mgr.containerPlugin != nil {
-			if err = mgr.containerPlugin.PostUpdate(c.BaseFS, c.Config.Env); err != nil {
-				return err
-			}
+	newEnvSlice, err := mergeEnvSlice(config.Env, c.Config.Env)
+	if err != nil {
+		return err
+	}
+	c.Config.Env = newEnvSlice
+	if mgr.containerPlugin != nil {
+		if err = mgr.containerPlugin.PostUpdate(c.BaseFS, c.Config.Env); err != nil {
+			return err
 		}
 	}
 
